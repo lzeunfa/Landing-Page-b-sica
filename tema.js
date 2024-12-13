b4bd1b02a83e74c08e8ffea83657e06a2486901f
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2024-12-12 21:24:53 -0300

    ajsutes pra adicionar mudança de tema

---
+++ tema.js
@@ -0,0 +1,12 @@
+var pt1 = document.getElementById(`pt1`)
+var lamp = 
+
+pt1.style.background=`#e4e4e4`
+
+function tema(){
+    if (pt1.style.background= `#e4e4e4`){
+        pt1.style.background= `#333333`
+    }else if(pt1.style.background= `#333333`){
+        pt1.style.background= `#e4e4e4`
+    }
+}
