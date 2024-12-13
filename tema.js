3d1ffdca713260c9ec87c995a2cbd44cb62e9993
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2024-12-12 21:39:43 -0300

    continua ajustes para alteração de tema

--- tema.js
+++ tema.js
@@ -1,11 +1,15 @@
 var pt1 = document.getElementById(`pt1`)
-var lamp = 
 
 pt1.style.background=`#e4e4e4`
+
+var lamp = document.getElementById(`lamp`)
+lamp.style.border=`1px solid black`
 
 function tema(){
     if (pt1.style.background= `#e4e4e4`){
         pt1.style.background= `#333333`
+        lamp.style.border=`1px solid white`
+
     }else if(pt1.style.background= `#333333`){
         pt1.style.background= `#e4e4e4`
     }
