f1019f592c722aa46c201d32b37aa97afe318b0d
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2024-12-12 23:10:36 -0300

    mais ajustes para mudança de tema

--- tema.js
+++ tema.js
@@ -1,16 +1,30 @@
+//pt1
 var pt1 = document.getElementById(`pt1`)
+pt1.style.backgroundColor=`#e4e4e4`
 
-pt1.style.background=`#e4e4e4`
-
+//ícone de lampada
 var lamp = document.getElementById(`lamp`)
 lamp.style.border=`1px solid black`
 
-function tema(){
-    if (pt1.style.background= `#e4e4e4`){
-        pt1.style.background= `#333333`
-        lamp.style.border=`1px solid white`
+//h1
+var h1pt1 = document.getElementById(`h1pt1`)
 
-    }else if(pt1.style.background= `#333333`){
-        pt1.style.background= `#e4e4e4`
+//p
+var ppt1 = document.getElementById(`ppt1`)
+
+
+function tema(){
+    if (pt1.style.backgroundColor= `#e4e4e4`){
+        pt1.style.backgroundColor= `#333333`
+        //
+        lamp.style.border=`1px solid white`
+        lamp.src=`img/lampbranca.png`
+        //
+        h1pt1.style.color=`#e4e4e4`
+        //
+        ppt1.style.color=`#e4e4e4`
+
+    }else if(pt1.style.backgroundColor= `#333333`){
+        pt1.style.backgroundColor= `#e4e4e4`
     }
 }
