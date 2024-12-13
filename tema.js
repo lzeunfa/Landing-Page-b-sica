861a3d282a3e97ec91067f8e8c3f1c07fe04604a
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2024-12-13 13:29:56 -0300

    adicionei a mudança de tema

--- tema.js
+++ tema.js
@@ -1,30 +1,34 @@
 //pt1
-var pt1 = document.getElementById(`pt1`)
-pt1.style.backgroundColor=`#e4e4e4`
+const pt1 = document.getElementById(`pt1`)
+
 
 //ícone de lampada
-var lamp = document.getElementById(`lamp`)
+const lamp = document.getElementById(`lamp`)
 lamp.style.border=`1px solid black`
 
 //h1
-var h1pt1 = document.getElementById(`h1pt1`)
+const h1pt1 = document.getElementById(`h1pt1`)
 
 //p
-var ppt1 = document.getElementById(`ppt1`)
+const ppt1 = document.getElementById(`ppt1`)
 
 
+//Função para alternar o tema
 function tema(){
-    if (pt1.style.backgroundColor= `#e4e4e4`){
-        pt1.style.backgroundColor= `#333333`
-        //
-        lamp.style.border=`1px solid white`
-        lamp.src=`img/lampbranca.png`
-        //
-        h1pt1.style.color=`#e4e4e4`
-        //
-        ppt1.style.color=`#e4e4e4`
+    pt1.classList.toggle(`dark-theme`)
+    pt1.classList.toggle(`light-theme`)
 
-    }else if(pt1.style.backgroundColor= `#333333`){
+    if(pt1.classList.contains(`dark-theme`)){
+        lamp.style.border = '1px solid white';
+        lamp.src = 'img/lampbranca.png';
+        pt1.style.backgroundColor= `#333333`
+        h1pt1.style.color = `#e4e4e4`
+        ppt1.style.color= `#e4e4e4`
+    }else{
+        lamp.style.border = '1px solid black';
+        lamp.src = 'img/Desk_table_study_electic_electrical_lamp_light_1_icon-icons.com_53497.png';
         pt1.style.backgroundColor= `#e4e4e4`
+        h1pt1.style.color = `#333333`
+        ppt1.style.color= `#333333`
     }
 }
